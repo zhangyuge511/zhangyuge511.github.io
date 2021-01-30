dae6a960037e3449dc81110044e0a0b6488447b7
zhangyuge511 <zhangyuge511>
2021-01-31 01:51:53 +0800

    add link game

--- main.js
+++ main.js
@@ -202,7 +202,7 @@ window.boot = function() {
     if (false) {
         BK.Script.loadlib();
     } else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.js?v=1';
         if (jsList) {
             jsList = jsList.map(function(x) {
                 return 'src/' + x;
