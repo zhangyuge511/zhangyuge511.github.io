60f013cc1d38acfc7f0ba97bfc9342b89c1f318e
zhangyuge511 <zhangyuge511>
2021-01-31 01:00:04 +0800

    add link game

--- linkup/js/config.js
+++ linkup/js/config.js
@@ -2,14 +2,14 @@ var config = (function(){
 
 
     var row = 8;
-    var col = 9;
-    var objectCount = 9;
+    var col = 7;
+    var objectCount = 14;
     var repeatCount = row * col / objectCount;
 
     var time = 120;
 
     var imgUrl = "./img/";
-    var imgExtension = ".jpg";
+    var imgExtension = ".png";
     var imgByName = function (name) {
         var src = imgUrl + name + imgExtension;
         return `<img draggable="false" src="${src}"></img>`;
